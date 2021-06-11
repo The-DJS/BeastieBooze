899fb32829e3ffd0b81458474a02e5b4de3e4777
David Franks <davidfrnks@gmail.com>
2021-06-11 17:46:43 -0500

    started to work on sole filters for reports/transactions.

--- client/src/reportsContext.jsx
+++ client/src/reportsContext.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext, createContext } from 'react';
 import axios from 'axios';
+import moment from 'moment';
 import { UserContext } from './userContext';
 
 const ReportsContext = createContext();
@@ -21,6 +22,26 @@ const ReportsContextProvider = ({ children }) => {
         .catch((err) => console.log(err))
     }
   }, [userInfo]);
+
+  const getWeekNum = () => {
+    const today = moment();
+    const month = today.month();
+    const year = today.year();
+    console.log(`Today: ${today}`);
+    console.log(`Month: ${month}`);
+    console.log(`Year: ${year}`);
+
+
+    // const first = moment("2021-06-01"); //saturday
+    // const nthOfMoth = Math.ceil(firstJun2021.date() / 7); //1
+
+    // const Jun112021 = moment("2021-06-17"); //saturday, the next one
+    // console.log(Math.ceil(Jun112021.date() / 7)); //prints 2, as expected
+  }
+
+  useEffect(() => {
+    getWeekNum();
+  }, [allTransactions])
 
   return (
     <ReportsContext.Provider
